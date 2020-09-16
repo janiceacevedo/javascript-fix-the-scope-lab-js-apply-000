f9c643532e8a069ae8fa2b1c802e44a4d2ef7395
blithe-debugger-5623 <janiceacevedo1@gmail.com>
2020-09-16 05:53:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var animal = 'dog'
+var animal = 'dog';
 
 function myAnimal() {
   // You should not need to modify this function
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
@@ -11,14 +11,14 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: Hard-coding 'cat' below will not work
-  return animal
+  return animal;
 }
 
 function add2(n) {
-  return n + two
+  const two = 2;
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
